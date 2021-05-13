74626ef09f01f4fb3d7b08e46f8d905786d3d81c
Anshu Raj <anshusopinion@gmail.com>
2021-05-13 21:38:27 +0530

    some thing changed

--- src/App.js
+++ src/App.js
@@ -1,46 +1,40 @@
 import { Button } from "@chakra-ui/button";
 import { Image } from "@chakra-ui/image";
-import { Link } from "@chakra-ui/layout";
-import {
-  Box,
-  Flex,
-  Heading,
-  List,
-  ListItem,
-  Stack,
-  Text,
-} from "@chakra-ui/layout";
+import { ListItem } from "@chakra-ui/layout";
+import { List } from "@chakra-ui/layout";
+import { Text } from "@chakra-ui/layout";
+import { Box, Flex, Heading, Stack } from "@chakra-ui/layout";
+import { getNodeText } from "@testing-library/dom";
 import { useState } from "react";
-
-function App() {
+const App = () => {
   const [gameStarted, setGameStarted] = useState(false);
-  const [selectedNumber, setSelectedNumber] = useState(null);
-  const [dice, setDice] = useState(0);
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [dice, setDice] = useState(1);
+  const [error, setError] = useState(null);
   const [score, setScore] = useState(0);
-  const [error, setError] = useState();
   const numbers = [1, 2, 3, 4, 5, 6];
-  const dices = [
-    { diceImage: "/dice/dice1.png", value: 1 },
-    { diceImage: "/dice/dice2.png", value: 2 },
-    { diceImage: "/dice/dice3.png", value: 3 },
-    { diceImage: "/dice/dice4.png", value: 4 },
-    { diceImage: "/dice/dice5.png", value: 5 },
-    { diceImage: "/dice/dice6.png", value: 6 },
-  ];
 
-  const generateRandomDiceNumber = () => {
+  const startGameHandler = () => {
+    setGameStarted(true);
+  };
+
+  const onNumberClicked = (value) => {
+    setSelectedNumber(value);
+    setError(null);
+  };
+
+  const genRandomNo = () => {
     if (selectedNumber) {
-      let genNumber = Math.ceil(Math.random() * 5) + 1;
-      setDice(genNumber - 1);
-      if (genNumber === selectedNumber) {
-        setScore((prev) => prev + genNumber);
-        setSelectedNumber(null);
+      const genratedNo = Math.ceil(Math.random() * 6);
+      setDice(genratedNo);
+
+      if (selectedNumber === genratedNo) {
+        setScore((prev) => prev + genratedNo);
       } else {
         setScore((prev) => prev - 2);
-        setSelectedNumber(null);
       }
     } else {
-      setError("Number not Selected ");
+      setError("Please Select Number");
     }
   };
 
@@ -49,65 +43,62 @@ function App() {
       {gameStarted ? (
         <>
           <Stack
+            justify="center"
             align="center"
-            maxW="1100px"
+            maxW="1300px"
             mx="auto"
             h="100vh"
-            justify="center"
           >
-            <Heading mb="8" color={error ? "red" : "black"}>
-              {" "}
+            <Heading
+              as="h1"
+              color={error ? "red" : "black"}
+              fontSize="6xl"
+              mb="8"
+            >
               {error ? error : "Select Number"}
             </Heading>
-
-            <Flex justify="space-between" w="600px">
-              {numbers.map((number, i) => (
+            <Flex pb="10">
+              {numbers.map((value) => (
                 <Flex
-                  width="50px"
-                  height="50px"
-                  bg={selectedNumber === number ? "gray" : "black"}
-                  color="white"
                   justify="center"
                   align="center"
-                  fontSize="3xl"
+                  h="50px"
+                  w="50px"
+                  bg={selectedNumber === value ? "green" : "black"}
+                  color="white"
+                  fontSize="2xl"
+                  key={value}
+                  mr={4}
                   borderRadius="md"
-                  key={i}
-                  onClick={() => {
-                    setSelectedNumber(number);
-                    setError(null);
-                  }}
+                  onClick={() => onNumberClicked(value)}
                 >
-                  {number}
+                  {value}
                 </Flex>
               ))}
             </Flex>
+            <Box h="150px" width="150px" onClick={genRandomNo}>
+              <Image src={`/dice/dice${dice}.png`} />
+            </Box>
 
-            <Stack pt="16" align="center">
-              <Box h="150px" width="150px" onClick={generateRandomDiceNumber}>
-                <Image src={dices[dice].diceImage} />
-              </Box>
-              <Text textAlign="center" fontSize="xl">
-                Click on dice to roll
-              </Text>
-            </Stack>
-            <Stack>
-              <Text
-                textAlign="center"
-                color={score < 0 ? "red" : "green"}
-                fontSize="8xl"
-                fontWeight="bold"
-              >
-                {score}
-              </Text>
-              <Text textAlign="center" fontSize="5xl" fontWeight="bold">
-                Total Score
-              </Text>
-              <Button onClick={() => setScore(0)}>Reset Score</Button>
-            </Stack>
+            <Text as="p" fontSize="xl">
+              Click on dice to roll
+            </Text>
+
+            <Text
+              color={score > 0 ? "green" : "red"}
+              fontSize="8xl"
+              fontWeight="bold"
+            >
+              {score}
+            </Text>
+            <Text fontSize="6xl" fontWeight="bold">
+              Total Score
+            </Text>
+            <Button onClick={() => setScore(0)}>Reset Score</Button>
           </Stack>
-          <Stack maxW="1100px" mx="auto" p="8">
-            <Heading>Game Rules:-</Heading>
-            <List listStyleType="disc">
+          <Stack maxW="900px" mx="auto">
+            <Heading as="h2">Game Rules:-</Heading>
+            <List>
               <ListItem>Select Number any number</ListItem>
               <ListItem>Click on dice image to roll it</ListItem>
               <ListItem>
@@ -115,44 +106,34 @@ function App() {
                 same point of dice
               </ListItem>
               <ListItem>
-                if You are Wrong Score will be deducted by 2 points
+                Select number is equal to obtained dice result then you will get
+                same point of dice
               </ListItem>
             </List>
-            <Box>
-              <Link href="https://youtube.com/dosomecoding">
-                {" "}
-                Join Channel{" "}
-              </Link>
-            </Box>
           </Stack>
         </>
       ) : (
-        <>
-          <Flex
-            maxW="1100px"
-            justify="center"
-            align="center"
-            mx="auto"
-            h="100vh"
-          >
-            <Image src="/dices.png" width="50%" />
-            <Stack>
-              <Heading fontSize="6xl">The Dice Game</Heading>
-              <Button
-                color="white"
-                bg="black"
-                alignSelf="flex-end"
-                _hover={{ bg: "grey" }}
-                onClick={() => setGameStarted(true)}
-              >
-                Play Now
-              </Button>
-            </Stack>
-          </Flex>
-        </>
+        <Flex justify="center" align="center">
+          <Image width="50%" src="/dices.png" />
+          <Stack>
+            <Heading fontSize="7xl" as="h1">
+              {" "}
+              The Dice Game
+            </Heading>
+            <Button
+              alignSelf="flex-end"
+              bg="black"
+              color="white"
+              _hover={{ bg: "grey" }}
+              onClick={startGameHandler}
+            >
+              Start Game
+            </Button>
+          </Stack>
+        </Flex>
       )}
     </>
   );
-}
+};
 
 export default App;
